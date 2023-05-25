2ccd1b004fff3aa77648e4bf45d10e1eeeb6f628
agartanah <vyatin2017@mail.ru>
2023-05-25 18:52:52 +0300

    Created a directory class. Made it possible to read files from the folder. Tested in Main

---
+++ Folder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8 {
+  public class Folder {
+    public string FolderPath { get; set; }
+    public DirectoryInfo FolderInfo { get; set; }
+    public List<FileInfo> FolderFiles;
+
+    public Folder(string FolderPath) {
+      this.FolderPath = FolderPath;
+      FolderInfo = new DirectoryInfo(FolderPath);
+      FolderFiles = ReadFolderFiles(FolderPath);
+    }
+
+    public List<FileInfo> ReadFolderFiles(string FolderPath) {
+      List<FileInfo> Files = new List<FileInfo>();
+
+      FileInfo[] FolderFilesArray = FolderInfo.GetFiles();
+
+      foreach (var File in FolderFilesArray) {
+        Files.Add(File);
+      }
+
+      return Files;
+    }
+
+    public void PrintFolderFiles() {
+      foreach (var File in FolderFiles) {
+        Console.WriteLine(File);
+      }
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8 {
+  class Program {
+    static void Main(string[] args) {
+      Folder Dictionary = new Folder(@"C:\Users\vyati\source\repos\Lab5\textfiles");
+
+      Dictionary.PrintFolderFiles();
+
+      Console.ReadKey();
+    }
+  }
+}
