2e361927a4448d493baca4c6381ca508391a1613
agartanah <vyatin2017@mail.ru>
2023-05-26 20:47:38 +0300

    I created a static class Presenter that implements methods to interact with the Folder class from the form. Directory Synchronization, Delete and Create Files methods

---
+++ Presenter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8 {
+  public static class Presenter {
+    public static bool Accept(string FolderBasePath, string FolderSyncPath) {
+      Folder FolderBase = new Folder(FolderBasePath);
+      Folder FolderSync = new Folder(FolderSyncPath);
+
+      return FolderBase.SynchronizationWithFolder(FolderSync.FolderInfo);
+    }
+
+    public static bool Delete(string FilePath) {
+      if (File.Exists(FilePath)) {
+        File.Delete(FilePath);
+        
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    public static bool Create(string FilePath) {
+      if (!File.Exists(FilePath)) {
+        File.Create(FilePath).Close();
+
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
